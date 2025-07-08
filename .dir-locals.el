adfc007916e3decb694b8a024950cabfb641b866
Daniel Pittman <daniel@rimspace.net>
2025-07-08 20:10:23 +1000

    fix compile-command for Emacs.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "cd ~/src/ONI-personal-mods/ && MSBuild SlippyCheezePersonalMods.sln -p:Configuration=Debug -p:Platform=\"Any CPU\" -p:ContinueOnError=true -p:WarningsAsErrors=true -p:MSBuildTreatWarningsAsErrors=true"))))
+((nil . ((compile-command . "cd ~/src/ONI-mods/ && MSBuild ONIMods.sln -p:Configuration=Debug -p:Platform=\"Any CPU\" -p:ContinueOnError=true -p:WarningsAsErrors=true -p:MSBuildTreatWarningsAsErrors=true"))))
