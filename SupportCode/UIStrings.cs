60de66bab104c7758839dc4f5b91078210b52231
Daniel Pittman <daniel@rimspace.net>
2025-07-17 13:59:22 +1000

    PLib / PBuilding all mods, implicitly

--- SupportCode/UIStrings.cs
+++ SupportCode/UIStrings.cs
@@ -13,7 +13,9 @@ public static class UIStrings {
     public static partial class links {
         // frequently used links.
         public static string Food(string label = "Food") => label.AsLink("FOOD");
+        public static string GasPipes(string label = "Gas Pipes") => label.AsLink("GASPIPING");
         public static string Gases(string label = "Gases") => label.AsLink(CodexEntryGenerator_Elements.ELEMENTS_GASES_ID);
+        public static string LiquidPipes(string label = "Liquid Pipes") => label.AsLink("LIQUIDPIPING");
         public static string Liquids(string label = "Liquids") => label.AsLink(CodexEntryGenerator_Elements.ELEMENTS_LIQUIDS_ID);
         public static string Solids(string label = "Solids") => label.AsLink(CodexEntryGenerator_Elements.ELEMENTS_SOLIDS_ID);
         public static string Temperature(string label = "Temperature") => label.AsLink("HEAT");
